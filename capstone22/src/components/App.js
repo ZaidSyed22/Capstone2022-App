0163e304b2c44a857a4948b9340c9bf2fcfd9423
ZaidSyed22 <sy3drahman2020@gmail.com>
2022-07-14 21:04:37 -0400

    added homepage, footer, navbar and added our pages together

--- capstone22/src/components/App.js
+++ capstone22/src/components/App.js
@@ -1,3 +1,13 @@
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import '../css/App.css';
+import '../css/Navbar.css';
+import Home from './Home';
+import Pagetemplate from './Pagetemplate';
 import PropertyInput from './PropertyInput';
 import Properties from './Properties';
 import Map from './map/Map';
@@ -8,13 +18,33 @@ function App() {
   return (
     <div className="App">
 
+<Router>
+      <Pagetemplate>
+        <Switch>
+
+    <Route path="/" exact>
+        <Home />
+      </Route>
+
+      <Route path="/PropertyInput.js" exact>  
       <InputProvider>
         <PropertyInput />
       </InputProvider>
+      </Route>
 
+      <Route path="/Properties.js" exact>  
       <Properties />
+      </Route>
 
+      <Route path="./map/Map.js" exact>  
       <Map />
+      </Route>
+    
+      
+  </Switch>
+</Pagetemplate>
+</Router>
+     
       
     </div>
   );
---
+++ capstone22/src/components/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../css/Home.css';
+
+
+function Home() {
+  return (
+    <div className= 'Home'> 
+        <form>       
+            <h1 class="text-small">
+            Welcome to Our Capstone 2022
+            </h1>   
+            <h4><b>Our Purpose:</b></h4>
+            <p>
+              .........................................
+            </p>
+            <b>
+            </b>
+            <p>          
+              .................................
+            </p>
+            <b>
+            </b>
+            <p>          
+            ........................
+            </p>
+        </form>
+    </div>
+        
+
+
+  )
+}
+
+export default Home;
